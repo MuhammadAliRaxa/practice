7874604d5605fbd9920c327e1ecce94d054586d2
MuhammadAliRaxa <muhammadaliraza7427@gmail.com>
2025-09-03 23:05:18 +0500

    Ahaaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animation_tween/liningPainter.dart';
 import 'package:flutter_animation_tween/splash_screen.dart';
 
 void main() {
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BotyCam',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -34,84 +33,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: SplashScreen(),
-    );
-  }
-}
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  double left=0;
-  double right=0;
-  double top=0;
-  double bottom=0;
-  double opacity=1.0;
-  void action()
-  {
-    setState(() {
-        if(top==0&&left==0){
-          setState(() {
-            top+=65;
-            left+=255;
-          });
-      }else if(top!=0){
-        setState(() {
-          top-=65;
-        });
-      }else if(left!=0)
-      {
-        top+=65;
-      left-=255;
-      }
-    });
-  }
-  @override
-  void initState() {
-    super.initState();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child:Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomPaint(
-            painter: LiningPainter(),
-            child: Container(
-              width: 300,
-              height: 100,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 10,
-                    child: AnimatedPositioned(
-                      top: top,
-                      left: left,
-                      duration: Duration(seconds: 2),
-                      child: Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(100)),
-                          color: Colors.black,
-                          )
-                        ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ) 
-      ,),
-      floatingActionButton: FloatingActionButton(onPressed: action),
     );
   }
 }
